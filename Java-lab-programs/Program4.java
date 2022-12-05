59650f0f7adb493f7115333b27e1cfcbd73c98a7
Prathiksha Kini <84091455+pkini2002@users.noreply.github.com>
2022-12-05 23:11:57 +0530

    Update Program4.java

--- Java-lab-programs/Program4.java
+++ Java-lab-programs/Program4.java
@@ -4,72 +4,54 @@
 // of program. Read a character from user and display the student names starting with given 
 // character.
 
-
 import java.util.Scanner;
 
-class Student{
+class student {
     String name;
     int rollno;
 
-    Student(String name,int rollno){
+    student(String name, int rollno){
         this.name=name;
         this.rollno=rollno;
     }
 
     void display(){
-        System.out.println("The name of the student is: "+name);
-        System.out.println("The roll number is: "+rollno);
+        System.out.println("Name of the student is: "+ name);
+        System.out.println("Roll number is: "+rollno);
     }
 }
 
-class Test extends student {
-    int marks1,marks2,marks3;
-    Test(int marks1,int marks2,int marks3,String name,int rollno){
-        super(name, rollno);
-        this.marks1=marks1;
-        this.marks2=marks2;
-        this.marks3=marks3;
-    }
-
-    void show_marks(){
-        System.out.println("Marks of subject 1 is: "+marks1);
-        System.out.println("Marks of subject 1 is: "+marks2);
-        System.out.println("Marks of subject 1 is: "+marks3);
-    }
-}
-
-interface Sports{
-    final int sports_marks=10;
-    void show_sportswt();
-}
-
-class Result extends Test implements Sports{
-    int total=marks1+marks2+marks3+sports_marks;
-
-    Result(int marks1,int marks2,int marks3,String name,int rollno){
-        super(marks1,marks2,marks3,name,rollno);
-    }
-
-    void display(){
-        System.out.println("Total marks is: "+total);
-    }
-
-    public void show_sportswt(){
-        System.out.println("Sports marks is: "+sports_marks);
-    }
-}
-public class Demo {
+public class Exception {
     public static void main(String args[]){
-         Scanner s=new Scanner(System.in);
-         System.out.println("Enter the name: ");
-         String name=s.nextLine();
-         System.out.println("Enter the roll number: ");
-         int rollno=s.nextInt();
-         System.out.println("Enter subject1,subject2,subject3 marks: ");
-         int sub1=s.nextInt();
-        int sub2=s.nextInt();
-        int sub3=s.nextInt();
-        Result r=new Result(sub1,sub2,sub3,name,rollno);
-        r.display();
+        String name;
+        int rollno;
+        Scanner s=new Scanner(System.in);
+        Scanner s2=new Scanner(System.in);
+
+        Student st[]=new Student[5];
+
+        try{
+            for(int i=0;i<5;i++){
+                System.out.println("Enter the name of the student: ");
+                name=s.nextLine();
+                System.out.println("Enter the rollnumber: ");
+                rollno=s2.nextInt();
+                st[i]=new Student(name,rollno);
+            }
+        }catch(ArrayIndexOutOfBoundsException e){
+            e.printStackTrace();
+        }
+
+        System.out.println("Enter the first character: ");
+        char check=s.next().charAt(0);
+        try{
+            for(int i=0;i<5;i++){
+                if(check == st[i].name.charAt(0)){
+                    st[i].display();
+                }
+            }
+        }catch(ArrayIndexOutOfBoundsException e){
+            e.printStackTrace();
+        }
     }
 }
