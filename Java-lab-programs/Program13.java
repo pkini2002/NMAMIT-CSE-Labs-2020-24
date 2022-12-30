c2ccac03e2c1a3c09239a2faf2feb3aa03cbc18f
Prathiksha Kini <84091455+pkini2002@users.noreply.github.com>
2022-12-30 17:51:09 +0530

    Update Program13.java

--- Java-lab-programs/Program13.java
+++ Java-lab-programs/Program13.java
@@ -1,5 +1,5 @@
-//Write a Java Program to implement the Comparator using TreeSet class of Java 
-//respectively and perform sorting operations on employee name and salary.
+//Write a Java Program to implement the Comparator using TreeSet class of Java
+//        respectively and perform sorting operations on employee name and salary.
 
 import java.util.*;
 
@@ -12,70 +12,64 @@ class Empl{
         this.salary=salary;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name=name;
-    }
-
-    public int getSalary() {
+    public int getSalary(){
         return salary;
     }
 
-    public void setSalary(int salary) {
+    public void setSalary(int salary){
         this.salary=salary;
     }
 
-    public String toString() {
-        return "\nName: "+this.name+" Salary: "+this.salary;
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public String toString(){
+        return "Name: "+this.name+" Salary: "+this.salary;
     }
 }
 
-public class Comp {
-    public static void main(String args[]) {
-        TreeSet<Empl>nameComp=new TreeSet<Empl>(new MyNameComp());
-        nameComp.add(new Empl("Ram",3000));
-        nameComp.add(new Empl("John",6000));
-        nameComp.add(new Empl("Crish",2000));
-        nameComp.add(new Empl("Tom",2400));
+public class TreeSetprog {
+    public static void main(String args[]){
+          TreeSet<Empl>nameComp=new TreeSet<Empl>(new MyNameComp());
+          nameComp.add(new Empl("Prathiksha",1000));
+          nameComp.add(new Empl("Pallavi",900));
+          nameComp.add(new Empl("Pooja",1200));
 
-        for(Empl e:nameComp) {
+          for(Empl e:nameComp){
+              System.out.println(e);
+          }
+
+          System.out.println("=================================");
+
+          TreeSet<Empl>salComp=new TreeSet<Empl>(new MySalaryComp());
+          salComp.add(new Empl("Prathiksha",1000));
+          salComp.add(new Empl("Pallavi",900));
+          salComp.add(new Empl("Pooja",1200));
+
+         for(Empl e:salComp){
             System.out.println(e);
-        }
+         }
 
-        System.out.println("=================");
-
-        TreeSet<Empl>salComp=new TreeSet<Empl>(new MySalaryComp());
-        salComp.add(new Empl("John",6000));
-        salComp.add(new Empl("Crish",2000));
-        salComp.add(new Empl("Tom",2400));
-        salComp.add(new Empl("Ram",3000));
-
-        System.out.println();
-        for(Empl e:salComp) {
-            System.out.println(e);
-        }
-
-        System.out.println("=================");
+        System.out.println("=================================");
     }
-}
 
-class MyNameComp implements Comparator<Empl>{
-    public int compare(Empl e1,Empl e2) {
-        return e1.getName().compareTo(e2.getName());
-    }
-}
-
-class MySalaryComp implements Comparator<Empl>{
-    public int compare(Empl e1, Empl e2)
-    {
-        if(e1.getSalary() > e2.getSalary()){
-            return 1;
+    static class MyNameComp implements Comparator<Empl>{
+        public int compare(Empl e1,Empl e2){
+            return e1.getName().compareTo(e2.getName());
         }
-        else {
-            return -1;
+    }
+
+    static class MySalaryComp implements Comparator<Empl>{
+        public int compare(Empl e1,Empl e2){
+            if(e1.salary > e2.salary)
+                return 1;
+            else
+                return -1;
         }
     }
 }
