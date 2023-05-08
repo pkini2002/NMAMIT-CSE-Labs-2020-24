5a459bd7e2b3bc0e4d5c8df34089b2315d0c15b0
Prathiksha Kini <84091455+pkini2002@users.noreply.github.com>
2023-05-08 23:54:44 +0530

    Update LeakyBucket.c

--- CN-Lab-Programs/LeakyBucket.c
+++ CN-Lab-Programs/LeakyBucket.c
@@ -1,90 +1,82 @@
-#include <stdio.h>
-#include <stdlib.h>
-#define BUCKETSIZE 250
+// Leaky bucket
+
+#include<stdio.h>
+#include<stdlib.h>
+
 #define OUTRATE 25
+#define BUCKET_SIZE 250
 
-struct
-{
-int arrtime;
-int weight;
-}packet [15];
+struct{
+    int arrtime;
+    int weight;
+}packet[15];
 
-void read_packets(int n)
-{
-int i;
-for (i=0;i<n;i++)
-{
-printf("Enter arrival time:");
-scanf("%d", &packet[i].arrtime);
-printf("Enter the packet size:");
-scanf("%d", &packet[i].weight);
-}
+void readpackets(int n){
+    for(int i=0;i<n;i++){
+        printf("Enter the arrival time: \n");
+        scanf("%d",&packet[i].arrtime);
+        printf("Enter the weight: \n");
+        scanf("%d",&packet[i].weight);
+    }
 }
 
-void lky_bucket()
-{
-int excess=BUCKETSIZE;
-int i,j=0, rem=0;
-for (i=0;i<=30;i++)
-{
-if (packet [j].arrtime==i)
-{
-if (packet [j].weight<=excess)
-{
-rem=packet [j].weight+rem;
-excess=excess-packet[j].weight;
+void leaky_bucket(){
+    int i,j=0,rem=0;
+    int excess=BUCKET_SIZE;
 
-printf("\nAt time-%d: packet %d inserted into bucket ",i, j+1);
-printf(" remaining bucket size- %d\n\n",excess);
-j=j+1;
-}
-else
-{
-printf("At timed-%d: packet %d discarded ",i,j+1);
-printf("Packet size is more than buffer size\n");
-j=j+1;
-}}
-if((i%5)==0)
-{
-if (rem>=OUTRATE)
-{
-rem=rem-OUTRATE;
-excess=excess+OUTRATE;
+    for(i=0;i<=30;i++){
+        if(packet[j].arrtime==i){
+            if(packet[j].weight <= excess){
+                rem+=packet[j].weight;
+                excess-=packet[j].weight;
+                printf("At time %d %d is inserted into the bucket\n",i,j+1);
+                printf("Remaining bucket size is %d\n",excess);
+                j=j+1;
+            }
+            else{
+                printf("At time %d the packet %d is discarded\n",i,j+1);
+                printf("Packet size is more than buffer size\n");
+                j+=1;
+            }
+        }
 
-printf("At timed= %d : 25 Kbytes are transfered ",i);
-printf("Free available space in the bucket-%d\n",excess);
-}
-else if(rem>0)
-{
-excess=excess+rem;
-printf("\nAt timed %d Kbytes are transferred bucket- %d ", excess);
+        // send packet every 5 millisec
+        if((i%5==0)){
+            if(rem>=OUTRATE){
+                rem-=OUTRATE;
+                excess+=OUTRATE;
+                printf("At time %d 25KBytes are transferred\n",i);
+            }
+            else if(rem > 0){
+                excess+=rem;
+                printf("At time %d , %d KBytes are transferred\n",i,rem);
+                printf("The free space available in the bucket is %d\n",excess);
+                rem=0;
+            }
+        }
 
-printf("Free available space in the in the bucket- %d\n",excess);
-rem=0;
-}}}
-while (rem != 0)
-{
-if (rem < OUTRATE)
-{
-excess=excess+rem;
-printf("At time = %d %d Kbytes are transfered\n",i++, rem);
-printf("Free available space in the bucket-%d\n", excess);
-break;
-}
-rem=rem-OUTRATE;
-excess=excess + OUTRATE;
-printf("\nAt time %d %d Kbytes are transfered\n", i++, OUTRATE);
-printf("Free available space in the bucket-%d\n", excess);
-}
-printf("\nBucket is empty\n");
-}
-int main()
-{
-int n;
-printf("Enter the number of packets:");
-scanf("%d",&n);
-read_packets (n);
-lky_bucket();
+    }
 
-return 0;
+      while(rem!=0){
+            if(rem<OUTRATE){
+                excess+=rem;
+                printf("At time %d , %d KBytes are transferred\n",i++,rem);
+                printf("The free space available in the bucket is %d\n",excess);
+                break;
+            }
+             rem-=OUTRATE;
+             excess+=OUTRATE;
+             printf("At time %d , %d KBytes are transferred\n",i++,OUTRATE);
+              printf("The free space available in the bucket is %d\n",excess);
+    }
+    printf("The bucket is empty\n");
+}
+
+int main(){
+   int n;
+   printf("Enter the number of packets: \n");
+   scanf("%d",&n);
+   readpackets(n);
+   leaky_bucket();
+   return 0;
 }
