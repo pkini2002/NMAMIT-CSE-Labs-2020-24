baf574c9d9ea63e927ac442528ad7b88e6a0da33
Prathiksha Kini <84091455+pkini2002@users.noreply.github.com>
2023-05-21 09:06:03 +0530

    Update RSA.c

--- CN-Lab-Programs/RSA.c
+++ CN-Lab-Programs/RSA.c
@@ -1,82 +1,99 @@
-#include <stdio.h>
-#include <stdlib.h>
-#define max 100
-int n,e,cipher[max],len;
-int gcd(int x,int y)
-{
-    while(x!=y)
-    {
-        if(x>y)
-        {
-            x-=y;
+//Write a program for simple RSA algorithm to encrypt and decrypt the data
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+
+#define MAX 100
+
+int cipher[MAX],n,e,len;
+
+int gcd(int x,int y){
+     while(x!=y){
+        if(x>y){
+            x-=y;
         }
-        else
-        {
+        else{
             y-=x;
         }
-    }
-    return x;
+     }
+     return x;
 }
-void sender()
-{
-   char ch[max];
-   int i,j;
-   printf("Enter message\n");
-   for(len=0;len<max;len++)
-   {
+
+void sender(){
+    char ch[MAX];
+    printf("\nEnter the message to be sent:\n");
+    for(len=0;len<MAX;len++){
         scanf("%c",&ch[len]);
         if(ch[len]=='\n')
             break;
-   }
-    printf("\nEncryption\n");
-    printf("n=%d, e=%d\n",n,e);
-    for(j=0;j<len;j++)
-    {
+    }
+    printf("\nEncryption..\n");
+    printf("n=%d\t\t e=%d\t\t",n,e);
+    for(int j=0;j<len;j++){
         cipher[j]=1;
-        for(i=0;i<e;i++)
-        {
+        for(int i=0;i<e;i++){
             cipher[j]=(cipher[j]*ch[j])%n;
         }
     }
-    for(i=0;i<len;i++)
-    {
+
+    for(int i=0;i<len;i++){
         printf("%d",cipher[i]);
     }
 }
-void receiver()
-{
-     int p=11,q=17,z,d,i,j;
-    long msg[max];
-    n=p*q;
-    z=(p-1)*(q-1);
-    e=1;
-    do{
-        e++;
-    }
-    while((gcd(e,z)!=1)&&(e<z));
-    for(d=2;d<z;d++)
-    {
-        if(((e*d)%z)==1)
-            break;
-    }
-    sender();
-    printf("\nDecryption\n");
-    for(j=0;j<len;j++)
-    {
-        msg[j]=1;
-        for(i=0;i<d;i++)
-            msg[j]=(msg[j]*cipher[j])%n;
-    }
-    for(i=0;i<len;i++)
-        printf("%c",msg[i]);
+
+void receiver(){
+   long msg[MAX];
+   int p=11;
+   int q=17;
+   n=p*q;
+   int z;
+   int d;
+   int i,j;
+   z=(p-1)*(q-1);
+   e=1;
+   do{
+    e++;
+   }while((gcd(e,z)!=1) && (e<z));
+
+   for(d=2;d<z;d++){
+      if(((e*d)%z) ==1){
+        break;
+      }
+   }
+
+   sender();
+
+   printf("\nDecryption..\n");
+   for(j=0;j<len;j++){
+       msg[j]=1;
+       for(i=0;i<d;i++){
+          msg[j]=(msg[j]*cipher[j])%n;
+       }
+   }
+
+   for(i=0;i<len;i++){
+      printf("%c",msg[i]);
+   }
 }
 
-int n,e,cipher[max],len;
-
-int main()
-{
+int main(){
     receiver();
     getchar();
     return 0;
 }
+
+
+/*
+
+Output:
+
+Enter the message to be sent:
+helloworld
+
+Encryption..
+n=187            e=3            59118808010010210013080111
+Decryption..
+helloworld
+
+*/
